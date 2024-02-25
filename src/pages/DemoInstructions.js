5ca4031238fdeb5dd2e4d6caf7069735e3db9e3a
Andrew Wang <andrew2014wang@gmail.com>
2024-02-25 15:03:22 -0800

    minor change

--- src/pages/DemoInstructions.js
+++ src/pages/DemoInstructions.js
@@ -12,21 +12,29 @@ function DemoInstructions() {
       <h3>Getting Started:</h3>
       <ol>
         <li>
+          <BlueLink to="/signup" text="Sign Up " /> or <BlueLink to="login" text="Log In " /> to access the demo application.
+        </li>
+        <br />
+        <li>
           <BlueLink to="/add_item" text="Select Test Accounts:" /> Choose one or more test bank accounts to add to your demo. This step enables you to explore transaction histories and account features without real-world financial implications.
         </li>
+        <br />
         <li>
           <BlueLink to="/Transaction" text="View Transactions:" /> Once you've added the bank accounts, navigate to the transactions page. Here, you can review detailed transaction data, analyze spending patterns, and gain insights into financial habits.
         </li>
+        <br />
         <li>
           <BlueLink to="/Merchant" text="View/Update Merchants and Merchant Types:"/> The Merchant page allows you to view and update
           all your merchants' type. You also have the option to create user-defined merchant types and update the type of any merchant.
            You can view transactions or create alerts based on any merchant type. (including user-defined type)
         </li>
+        <br />
         <li>
             <BlueLink to="/Rule" text="Confgure Alert Rule:" /> Visit the rules page to set up spending alerts. 
             This feature allows you to create personalized notifications based on specific spending behaviors, 
             helping you manage finances more effectively.
         </li>
+        <br />
         <li>
             <BlueLink to="/Alert" text="View Alerts:" />The Alert page shows all alerts triggered by the rules. 
         </li>
